f67dd6e3a6c7436fc998044941bc7baea9e898ae
james <me@jamescr.dev>
2023-02-28 12:31:12 -0500

    lab: tf variables pg71

--- labs/Terraform Variables/main.tf
+++ labs/Terraform Variables/main.tf
@@ -142,3 +142,25 @@ resource "aws_instance" "web_server" {                            # BLOCK
     Name = "Web EC2 Server"
   }
 }
+
+# resource "aws_subnet" "variables-subnet" {
+#   vpc_id                  = aws_vpc.vpc.id
+#   cidr_block              = "10.0.250.0/24"
+#   availability_zone       = "us-east-1a"
+#   map_public_ip_on_launch = true
+#   tags = {
+#     Name      = "sub-variables-us-east-1a"
+#     Terraform = "true"
+#   }
+# }
+
+resource "aws_subnet" "variables-subnet" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.variables_sub_cidr
+  availability_zone       = var.variables_sub_az
+  map_public_ip_on_launch = var.variables_sub_auto_ip
+  tags = {
+    Name      = "sub-variables-${var.variables_sub_az}"
+    Terraform = "true"
+  }
+}
