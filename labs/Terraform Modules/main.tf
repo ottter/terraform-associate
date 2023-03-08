fb77a3e698e9b3a8881a6a1e8114afed3591a020
james <me@jamescr.dev>
2023-03-08 14:01:24 -0500

    lab: tf modules

--- labs/Terraform Modules/main.tf
+++ labs/Terraform Modules/main.tf
@@ -330,3 +330,24 @@ output "public_ip_server_subnet_1" {
 output "public_dns_server_subnet_1" {
   value = aws_instance.web_server.public_dns
 }
+
+# module "server" {
+#   source    = "./modules/server"
+#   ami       = data.aws_ami.ubuntu.id
+#   subnet_id = aws_subnet.public_subnets["public_subnet_3"].id
+#   security_groups = [aws_security_group.vpc-ping.id,
+#     aws_security_group.ingress-ssh.id,
+#   aws_security_group.vpc-web.id]
+# }
+
+module "server_subnet_1" {
+  source      = "./modules/web_server"
+  ami         = data.aws_ami.ubuntu.id
+  key_name    = aws_key_pair.generated.key_name
+  user        = "ubuntu"
+  private_key = tls_private_key.generated.private_key_pem
+  subnet_id   = aws_subnet.public_subnets["public_subnet_1"].id
+  security_groups = [aws_security_group.vpc-ping.id,
+    aws_security_group.ingress-ssh.id,
+  aws_security_group.vpc-web.id]
+}
