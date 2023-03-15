f66cf0183dd135e6172243060f7a1124b4087013
james <me@jamescr.dev>
2023-03-15 17:01:59 -0400

    pg200 - tf workflow

--- labs/Terraform Module Registry/main.tf
+++ labs/Terraform Module Registry/main.tf
@@ -382,3 +382,11 @@ output "asg_group_size" {
   value = module.autoscaling.autoscaling_group_max_size
 }
 
+module "s3-bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "2.11.1"
+}
+
+output "s3_bucket_name" {
+  value = module.s3-bucket.s3_bucket_bucket_domain_name
+}
--- labs/Terraform Module Scope/main.tf
+++ labs/Terraform Module Scope/main.tf
@@ -376,4 +376,8 @@ module "autoscaling" {
     Name = "Web EC2 Server 2"
   }
 
+  output "asg_group_size" {
+value = module.autoscaling.autoscaling_group_max_size
+}
+
 }
