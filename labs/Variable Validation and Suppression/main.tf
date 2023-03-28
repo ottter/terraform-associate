713ff1651aae80ad04bcfec5ee0eb408dda59f2c
james <me@jamescr.dev>
2023-03-28 15:31:32 -0400

    variable validation pg319

--- labs/Variable Validation and Suppression/main.tf
+++ labs/Variable Validation and Suppression/main.tf
@@ -9,6 +9,37 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "phone_number" {
+  type      = string
+  sensitive = true
+  default   = "867-5309"
+}
+locals {
+  contact_info = {
+    cloud        = var.cloud
+    department   = var.no_caps
+    cost_code    = var.character_limit
+    phone_number = var.phone_number
+  }
+  my_number = nonsensitive(var.phone_number)
+}
+output "cloud" {
+  value = local.contact_info.cloud
+}
+output "department" {
+  value = local.contact_info.department
+}
+output "cost_code" {
+  value = local.contact_info.cost_code
+}
+output "phone_number" {
+  value = local.contact_info.phone_number
+}
+output "my_number" {
+  value = local.my_number
+}
+
+
 locals {
   team        = "api_mgmt_dev"
   application = "corp_api"
@@ -30,7 +61,7 @@ locals {
     Service   = local.service_name
     AppTeam   = local.app_team
     CreatedBy = local.createdby
- } 
+  }
 }
 
 #Retrieve the list of AZs in the current AWS region
